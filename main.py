70f2f13cbf993035ca9263dacac5b8863ff5dad5
EmilyG27 <133466995+EmilyG27@users.noreply.github.com>
2025-01-05 23:34:32 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import mood_responses
+import text_utils
 
 def main():
         mood = input("How are you feeling? ")
         print(mood_responses.mood_response(mood))
 
+reverse_message = "Message"
+capitalize_message = "message"
+
+print(text_utils.reverse_string(reverse_message))
+print(text_utils.capitalize_string(capitalize_message))
+
 if __name__ == "__main__":
     main()
+
---
+++ text_utils.py
@@ -0,0 +1,5 @@
+def reverse_string(message):
+    return message[::-1]
+
+def capitalize_string(caps):
+    return caps.capitalize()
